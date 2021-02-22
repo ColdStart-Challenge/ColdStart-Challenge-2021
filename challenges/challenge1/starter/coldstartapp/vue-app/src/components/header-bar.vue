1a05ed34bf29a16b369661c8bddf74d3ddb5dc30
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-22 21:21:44 +0100

    Fix Errors

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
@@ -1,7 +1,7 @@
 <script>
 import HeaderBarBrand from '@/components/header-bar-brand.vue';
 
-import axios from 'axios';
+// import axios from 'axios';
 import API from '../store/config';
 
 export default {
@@ -10,20 +10,19 @@ export default {
     HeaderBarBrand,
   },
   data() {
-      try {
-    console.log('getUserInfo');
-    const response = fetch(`${API}/.auth/me`);
-    const payload = response.json();
-    const { clientPrincipal } = payload;
-    console.log(clientPrincipal);
-    // return clientPrincipal;
-    return {
-      userInfo: clientPrincipal
-    };
-  } catch (error) {
-    console.error('No profile could be found');
-    return undefined;
-  }
+    try {
+      console.log('getUserInfo');
+      const response = fetch(`${API}/.auth/me`);
+      const payload = response.json();
+      const { clientPrincipal } = payload;
+      console.log(clientPrincipal);
+      return {
+        userInfo: clientPrincipal
+      };
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
   },
   methods: {
   },
@@ -37,8 +36,12 @@ export default {
       <div class="navbar-menu">
         <div class="navbar-start">
           <router-link class="navbar-item nav-home" to="/">Home</router-link>
-          <div v-if="clientPrincipal"><router-link class="navbar-item nav-auth-link" to="/login">Login</router-link></div>
-          <div v-if="!clientPrincipal"><router-link class="navbar-item nav-auth-link" to="/logout">logout</router-link></div>
+          <div v-if="clientPrincipal">
+            <router-link class="navbar-item nav-auth-link" to="/login">Login</router-link>
+            </div>
+          <div v-if="!clientPrincipal">
+            <router-link class="navbar-item nav-auth-link" to="/logout">logout</router-link>
+          </div>
         </div>
       </div>
     </nav>
