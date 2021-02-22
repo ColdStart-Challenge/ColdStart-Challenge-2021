2a26caa45007165aa04aca766d458a587ae2b424
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-22 19:47:12 +0100

    Get UserInfo

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
@@ -1,13 +1,29 @@
 <script>
 import HeaderBarBrand from '@/components/header-bar-brand.vue';
 
+import axios from 'axios';
+import API from '../store/config';
+
 export default {
   name: 'HeaderBar',
   components: {
     HeaderBarBrand,
   },
   data() {
+      try {
+    console.log('getUserInfo');
+    const response = await fetch(`${API}/.auth/me`);
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+    console.log(clientPrincipal);
+    // return clientPrincipal;
+  } catch (error) {
+    console.error('No profile could be found');
+    // return undefined;
+  }
+
     return {
+      "userInfo": clientPrincipal
     };
   },
   methods: {
@@ -22,8 +38,8 @@ export default {
       <div class="navbar-menu">
         <div class="navbar-start">
           <router-link class="navbar-item nav-home" to="/">Home</router-link>
-          <router-link class="navbar-item nav-auth-link" to="/login">Login</router-link>
-          <router-link class="navbar-item nav-auth-link" to="/logout">logout</router-link>
+          <div v-if="clientPrincipal"><router-link class="navbar-item nav-auth-link" to="/login">Login</router-link></div>
+          <div v-if="!clientPrincipal"><router-link class="navbar-item nav-auth-link" to="/logout">logout</router-link></div>
         </div>
       </div>
     </nav>
