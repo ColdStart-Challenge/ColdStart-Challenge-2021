4e555f79aa5e4662be22e70b3a6711244219bb84
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-23 10:33:55 +0100

    Review Check

--- challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/components/header-bar.vue
@@ -41,10 +41,10 @@ export default {
       <div class="navbar-menu">
         <div class="navbar-start">
           <router-link class="navbar-item nav-home" to="/">Home</router-link>
-          <router-link v-if="!isAuthenticated" class="navbar-item nav-auth-link" to="/login">
+          <router-link v-if="isAuthenticated === true" class="navbar-item nav-auth-link" to="/logout">
+            Logout</router-link>
+          <router-link v-else class="navbar-item nav-auth-link" to="/login">
             Login</router-link>
-          <router-link v-if="isAuthenticated" class="navbar-item nav-auth-link" to="/logout">
-            logout</router-link>
         </div>
       </div>
     </nav>
