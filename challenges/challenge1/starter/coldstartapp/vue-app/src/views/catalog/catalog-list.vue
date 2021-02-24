bdead3ff6d69c00755c7b64927beada1ff9d98bf
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-24 11:43:49 +0100

    add user info stuff
    compose return object

--- challenges/challenge1/starter/coldstartapp/vue-app/src/views/catalog/catalog-list.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/views/catalog/catalog-list.vue
@@ -1,6 +1,7 @@
 <script>
 import CardContent from '@/components/card-content.vue';
 import ButtonFooter from '@/components/button-footer.vue';
+import getUserInfo from '@/assets/js/userInfo';
 
 export default {
   name: 'CatalogList',
@@ -20,44 +21,43 @@ export default {
   },
   data() {
     return {
+      isAuthenticated: false,
+      userDetails: 'unknown',
     };
+  },
+  mounted() {
+    this.getIsAuthenticated();
   },
   methods: {
     clicked: (item) => {
       console.log(item);
-
-      // const o = JSON.parse(item);
-      // console.log(o);
-
-      // {
-      //     "Id": "0B476647-586A-EB11-9889-000D3AB17657",
-      //     "User": "Pip Doe",
-      //     "Date": "2021-02-08T21:54:56.260Z",
-      //     "IcecreamId": 1,
-      //     "Status": "New",
-      //     "DriverId": null,
-      //     "FullAddress": "1 Microsoft Way, Redmond, WA 98052, USA",
-      //     "LastPosition": null
-      // }
-
       if (item.Id) {
-      // if (o.Id) {
         console.log('Valid submit event payload!');
+        const ret = {
+          Id: item.Id,
+          User: 'Pip Doe',
+          Date: '2021-02-08T21:54:56.260Z',
+          IcecreamId: 1,
+          Status: 'New',
+          DriverId: null,
+          FullAddress: '1 Microsoft Way, Redmond, WA 98052, USA',
+          LastPosition: null,
+        };
+        console.log(ret);
         return true;
       }
       console.warn('Invalid submit event payload!');
       return false;
     },
-    clicked2(item) {
-      console.log(item);
-      const o = JSON.parse(item);
-      console.log(o);
-      if (item.Id) {
-        console.log('Valid submit event payload!');
-        return true;
-      }
-      console.warn('Invalid submit event payload!');
-      return false;
+    getIsAuthenticated() {
+      getUserInfo().then((r) => {
+        console.log(r);
+        this.isAuthenticated = (r !== null);
+        this.userDetails = r.clientPrincipal.userDetails;
+      },
+      () => {
+        this.userDetails = 'unknown';
+      });
     },
   },
 };
