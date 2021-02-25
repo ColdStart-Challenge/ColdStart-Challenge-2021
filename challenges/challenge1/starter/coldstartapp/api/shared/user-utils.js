30da3c9d686792db840544b69c9d351a9e0391af
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-25 21:34:31 +0100

    Lint Issue

--- challenges/challenge1/starter/coldstartapp/api/shared/user-utils.js
+++ challenges/challenge1/starter/coldstartapp/api/shared/user-utils.js
@@ -1,13 +1,15 @@
 const getUser = (req) => {
-    const header = req.headers["x-ms-client-principal"];
-    if (header != undefined) {
-        const encoded = Buffer.from(header, "base64");
-        const decoded = encoded.toString("ascii");
+    // const header = req.headers["x-ms-client-principal"];
+    // if (header != undefined) {
+    //     const encoded = Buffer.from(header, "base64");
+    //     const decoded = encoded.toString("ascii");
 
-        return JSON.parse(decoded);
-    } else {
-        return { userDetails: "John Doe" };
-    }
+    //     return JSON.parse(decoded);
+    // } else {
+    //     return { userDetails: "John Doe" };
+    // }
+
+    return { userDetails: "John Doe" };
 };
 
 module.exports = { getUser };
