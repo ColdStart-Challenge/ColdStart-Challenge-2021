c84692c36de061571996e463958fda09a026cfd1
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-25 10:00:08 +0100

    Step 1

--- challenges/challenge1/starter/coldstartapp/api/orders-post/index.js
+++ challenges/challenge1/starter/coldstartapp/api/orders-post/index.js
@@ -14,37 +14,37 @@ function getGUID() {
 }
 
 module.exports = async function (context, req) {
-  // // Get the user details from the request
-  // const user = getUser(req);  
+  // Get the user details from the request
+  const user = "sujithq"; //getUser(req);  
 
   // // Get the pre-order from the request
 
-  // const ret = {
-  //   Id: getGUID().toUpperCase(),
-  //   User: user,
-  //   Date: new Date().toISOString(),
-  //   IcecreamId: context.IcecreamId,
-  //   Status: 'New',
-  //   DriverId: null,
-  //   FullAddress: '1 Microsoft Way, Redmond, WA 98052, USA',
-  //   LastPosition: null,
-  // };
+  const ret = {
+    // Id: getGUID().toUpperCase(),
+    User: user,
+    Date: new Date().toISOString(),
+    IcecreamId: context.IcecreamId,
+    Status: 'New',
+    DriverId: null,
+    FullAddress: '1 Microsoft Way, Redmond, WA 98052, USA',
+    LastPosition: null,
+  };
   
   // // TODO: add the pre-order JSON document in a queue
 
 
-  // // Retrieve the connection from an environment
-  // // variable called AZURE_STORAGE_CONNECTION_STRING
-  // const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+  // Retrieve the connection from an environment
+  // variable called AZURE_STORAGE_CONNECTION_STRING
+  const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
 
-  // // Create a unique name for the queue
-  // const queueName = 'preorder';
+  // Create a unique name for the queue
+  const queueName = 'preorder';
 
-  // console.log('Preordering queue: ', queueName);
+  console.log('Preordering queue: ', queueName);
 
-  // // Instantiate a QueueServiceClient which will be used
-  // // to create a QueueClient and to list all the queues
-  // const queueServiceClient = QueueServiceClient.fromConnectionString(connectionString);
+  // Instantiate a QueueServiceClient which will be used
+  // to create a QueueClient and to list all the queues
+  const queueServiceClient = QueueServiceClient.fromConnectionString(connectionString);
 
   // // Get a QueueClient which will be used
   // // to create and manipulate a queue
