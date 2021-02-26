d76aa1e186841e6fb57ad7c37ce6d9a891d9c785
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-26 14:25:34 +0100

    fix indentation

--- challenges/challenge1/starter/coldstartapp/vue-app/src/store/modules/order.js
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/store/modules/order.js
@@ -6,39 +6,39 @@ import { parseItem } from './action-utils';
 const captains = console;
 
 export default {
-    strict: process.env.NODE_ENV !== 'production',
-    namespaced: true,
-    state: {
-      order: {},
+  strict: process.env.NODE_ENV !== 'production',
+  namespaced: true,
+  state: {
+    order: {},
+  },
+  mutations: {
+    [POST_ORDER](state, order) {
+      state.order = order;
     },
-    mutations: {
-      [POST_ORDER](state, order) {
-        state.order = order;
-      },
+  },
+  actions: {
+    async postOrderAction({ commit }, ret) {
+      console.log("Enter postOrderAction");
+      console.log(ret);
+      try {
+        captains.log("Enter postOrderAction");
+        captains.log("Before POST");
+        const response = await axios.post(`${API}/orders`, ret);
+        captains.log("After POST");
+        captains.log("Before Parse");
+        const order = parseItem(response);
+        captains.log("After Parse");
+        captains.log(order);
+        commit(POST_ORDER, order);
+        captains.log("Leave postOrderAction");
+        return order;
+      } catch (error) {
+        captains.error(error);
+        throw new Error(error);
+      }
     },
-    actions: {
-      async postOrderAction({ commit }, ret) {
-        console.log("Enter postOrderAction");
-        console.log(ret);
-        try {
-            captains.log("Enter postOrderAction");
-            captains.log("Before POST");
-            const response = await axios.post(`${API}/orders`, ret);
-            captains.log("After POST");
-            captains.log("Before Parse");
-            const order = parseItem(response);
-            captains.log("After Parse");
-            captains.log(order);
-            commit(POST_ORDER, order);
-            captains.log("Leave postOrderAction");
-            return order;
-        } catch (error) {
-            captains.error(error);
-            throw new Error(error);
-        }
-      },
-    },
-    getters: {
-      order: (state) => state.order,
-    },
-  };
+  },
+  getters: {
+    order: (state) => state.order,
+  },
+};
