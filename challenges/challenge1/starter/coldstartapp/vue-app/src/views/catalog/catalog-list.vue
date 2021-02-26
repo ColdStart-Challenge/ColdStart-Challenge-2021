3c6c34d4991bb9216877639730dc415900029132
Sujith Quintelier <sujith.quintelier@gmail.com>
2021-02-26 11:47:03 +0100

    missing ;

--- challenges/challenge1/starter/coldstartapp/vue-app/src/views/catalog/catalog-list.vue
+++ challenges/challenge1/starter/coldstartapp/vue-app/src/views/catalog/catalog-list.vue
@@ -53,7 +53,7 @@ export default {
           return true;
         } catch (error) {
           console.log(error);
-          return false
+          return false;
         }
       }
       console.warn('Invalid submit event payload!');
