fbf904bd4e7606bf6f8e4c84768ce33044ec90ef
ntrogh <ntrogh@hotmail.com>
2021-04-26 16:22:02 +0200

    Add Blazor starter app

---
+++ challenges/challenge4/starter/ColdStartCustomerSWA/ColdStartCustomerSWA.Shared/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ColdStartCustomerSWA
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+
+        public string User { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public IceCream IceCream { get; set; }
+
+        public string Status { get; set; }
+
+        public Driver Driver { get; set; }
+
+        public string FullAddress { get; set; }
+
+        public string DeliveryPosition { get; set; }
+
+        public string LastPosition { get; set; }
+    }
+}
